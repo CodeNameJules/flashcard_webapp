6b6e94816234cd5887e05e964bc8a1e52e0a0987
“Julia <01julia.flato@gmail.com>
2025-02-18 17:59:27 +0000

    Set up database

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # db will be used to interact with database
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) # represents the name of the file that was ran(?)
     app.config['SECRET_KEY'] = 'superdupersecret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app) # takes database to use with app
 
     from .views import views
     from .auth import auth
---
+++ website/models.py
@@ -0,0 +1,25 @@
+from . import db # importing from this package (website folder)
+from flask_login import UserMixin # Flask module that makes login simpler
+from sqlalchemy.sql import func # if i want auto populated date
+
+class Flashcard(db.Model): # Model is like a blueprint for the class, ie that all flashcards need to look like this
+    id = db.Column(db.Integer, primary_key=True) # default is that the id is auto incremented
+    question = db.Column(db.String(300))
+    answer = db.Column(db.String(300))
+    score = db.Column(db.Integer)
+    # date = db.Column(db.DateTime(timezone=True), default=func.now())
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'))
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    # date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    flashcards = db.relationship('Flashcards') # creates a relationship with the Flashcard class, will store a list of all the cards
+
+class User(db.Model, UserMixin): # inherit db model and UserMixin
+    id = db.Column(db.Integer, primary_key=True) # db.Integer sets the type
+    email = db.Column(db.String(150), unique=True) # number in db.String() is the char limit
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    decks = db.relationship('Deck') # creates a relationship with the Deck class, will store a list of all their decks
